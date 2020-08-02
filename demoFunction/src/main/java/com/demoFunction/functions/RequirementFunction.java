15675a4ffd2dfefe2d95a2ac7cf1cbdabecddd37
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2020-08-01 19:00:45 -0500

    refactor

--- demoFunction/src/main/java/com/demoFunction/functions/RequirementFunction.java
+++ demoFunction/src/main/java/com/demoFunction/functions/RequirementFunction.java
@@ -4,15 +4,21 @@ import com.demoFunction.model.RequirementRequest;
 import com.demoFunction.model.RequirementResponse;
 import com.demoFunction.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component
-@AllArgsConstructor
 public class RequirementFunction implements Function<RequirementRequest, RequirementResponse> {
 
-    private CompanyService companyService;
+
+    private final CompanyService companyService;
+
+    @Autowired
+    public RequirementFunction(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 
     @Override
     public RequirementResponse apply(RequirementRequest requirementRequest) {
